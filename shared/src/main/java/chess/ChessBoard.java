a600e2688f28baa9bf95b29d1787d0ea106bf605
Ranger223 <steven13ates@gmail.com>
2024-01-13 23:28:00 -0700

    Passing Bishops move to edge case

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -21,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        chessBoard[position.getRow()][position.getColumn()] = piece;
+        chessBoard[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -32,7 +32,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        return chessBoard[position.getRow()][position.getColumn()];
+        return chessBoard[position.getRow() - 1][position.getColumn() - 1];
     }
 
     /**
