53406de6aee8454bfcceb8642027693ec1d33a52
Ranger223 <steven13ates@gmail.com>
2024-03-07 21:14:07 -0700

    Added even more tests.

--- server/src/test/java/dataAccessTests/AuthDAOTests.java
+++ server/src/test/java/dataAccessTests/AuthDAOTests.java
@@ -1,4 +1,68 @@
 package dataAccessTests;
 
+import dataAccess.*;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class AuthDAOTests {
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
+    @BeforeEach
+    public void setUp() {
+        authDAO = new SqlAuthDAO();
+        gameDAO = new SqlGameDAO();
+        userDAO = new SqlUserDAO();
+
+        authDAO.clear();
+        gameDAO.clear();
+        userDAO.clear();
+    }
+
+    @Test
+    public void testClear() throws DataAccessException {
+        AuthData auth = new AuthData("test", "token");
+        authDAO.createAuth(auth);
+
+        Assertions.assertDoesNotThrow(() -> {
+            gameDAO.clear();
+        });
+    }
+
+    @Test
+    public void testCreateAuthPos() throws DataAccessException {
+        AuthData auth = new AuthData("test", "token");
+
+
+        Assertions.assertDoesNotThrow(() -> {
+            authDAO.createAuth(auth);
+        });
+    }
+
+    @Test
+    public void testCreateUserNeg() throws DataAccessException {
+        AuthData auth = new AuthData(null, "token");
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            authDAO.createAuth(auth);
+        });
+    }
+
+    @Test
+    public void testGetUserPos() throws DataAccessException {
+        UserData user = new UserData("test", "pass", "email");
+        userDAO.createUser(user);
+
+        Assertions.assertNotNull(userDAO.getUser("test"));
+    }
+
+    @Test
+    public void testGetUserNeg() throws DataAccessException {
+        UserData user = new UserData("test", "pass", "email");
+        userDAO.createUser(user);
+        Assertions.assertNull(userDAO.getUser("yomama"));
+    }
 }
--- server/src/test/java/dataAccessTests/UserDAOTests.java
+++ server/src/test/java/dataAccessTests/UserDAOTests.java
@@ -1,4 +1,67 @@
 package dataAccessTests;
 
+import chess.ChessGame;
+import dataAccess.*;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class UserDAOTests {
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
+    @BeforeEach
+    public void setUp() {
+        authDAO = new SqlAuthDAO();
+        gameDAO = new SqlGameDAO();
+        userDAO = new SqlUserDAO();
+
+        authDAO.clear();
+        gameDAO.clear();
+        userDAO.clear();
+    }
+
+    @Test
+    public void testClear() throws DataAccessException {
+        UserData user = new UserData("test", "pass", "email");
+        userDAO.createUser(user);
+
+        Assertions.assertDoesNotThrow(() -> {
+            gameDAO.clear();
+        });
+    }
+
+    @Test
+    public void testCreateUserPos() throws DataAccessException {
+        UserData user = new UserData("test", "pass", "email");
+
+        Assertions.assertDoesNotThrow(() -> {
+            userDAO.createUser(user);
+        });
+    }
+
+    @Test
+    public void testCreateUserNeg() throws DataAccessException {
+        UserData user = new UserData(null, "pass", "email");
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            userDAO.createUser(user);
+        });
+    }
+
+    @Test
+    public void testGetUserPos() throws DataAccessException {
+        UserData user = new UserData("test", "pass", "email");
+        userDAO.createUser(user);
+
+        Assertions.assertNotNull(userDAO.getUser("test"));
+    }
+
+    @Test
+    public void testGetUserNeg() throws DataAccessException {
+        UserData user = new UserData("test", "pass", "email");
+        userDAO.createUser(user);
+        Assertions.assertNull(userDAO.getUser("yomama"));
+    }
 }
